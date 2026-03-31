5d5bf6c40683a6307fee1e16cd294f3495472fe8
hein101-ctrl <jordaanlh@hotmail.com>
2026-03-31 15:24:23 +0200

    Delete estate28_event_manager directory

--- estate28_event_manager/estate28_event_manager/estate28_event_manager/doctype/estate_28_event/estate_28_event.js
+++
@@ -1,6 +0,0 @@
-frappe.ui.form.on("Estate 28 Event", {
-    refresh(frm) {
-        // placeholder for future custom buttons
-        // e.g. Create Sales Invoice, Create Customer, WhatsApp pack, etc.
-    }
-});
